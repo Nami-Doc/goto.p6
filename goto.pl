016978f2246bb075343ad37ed277ab5d20e9983e
Nami-Doc <vendethiel@hotmail.fr>
2014-08-20 14:22:17 +0200

    Seems like I failed at git.

--- goto.pl
+++ goto.pl
@@ -1,2 +1,31 @@
 #!/usr/bin/env perl6
-my %paths = 
+constant FILE = 'paths.txt';
+my %paths = slurp(FILE).lines>>.split('=');
+
+sub save-paths {
+  my $content = %paths.map({ "$(.key)=$(.value)" }).join("\n");
+  spurt(FILE, $content);
+}
+
+multi MAIN {
+  say "Paths defined: ";
+  say;
+  say "  $(.key) => $(.value)" for %paths;
+}
+
+multi MAIN(Str $key, Str $value, Bool :$add!) {
+  %paths{$key} = $value;
+  save-paths;
+}
+
+multi MAIN(Str $key, Bool :$remove!) {
+  die "No such path $key" unless %paths{$key}:exists;
+  %paths{$key}:delete;
+  save-paths;
+}
+
+multi MAIN($key) {
+  die "No such path $key" unless %paths{$key}:exists;
+  say "Going to %paths{$key}";
+  shell "cd %paths{$key}";
+}
